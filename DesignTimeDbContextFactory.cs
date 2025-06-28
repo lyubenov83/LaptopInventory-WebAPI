4ffe2ed2237b7039f2911de3c1867aa3b5147633
Svetlin Lyubenov <svetlinlyubenov83@gmail.com>
2025-06-28 13:51:26 +0300

    Initial commit - clean Web API version

---
+++ DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using LaptopInventoryAPI.Data;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System;
+
+namespace LaptopInventoryAPI.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseMySql(
+                "server=localhost;port=3306;user=root;password=Summerof2025;database=laptopdb",
+                new MySqlServerVersion(new Version(8, 0, 36))
+            );
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
